7343b5f91c08308bb05649273f387e1dc88f1bf1
Vigneshwaran918@gmail.com <bvigneshwaran918@gmail.com>
2024-09-26 21:36:14 +0530

    first import

---
+++ src/main/java/org/example/nestedfor.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class nestedfor {
+    public static void main(String[] args) {
+
+        for(int j=1;j<=3;j++){
+
+        for (int i = 1; i <= 3; i++) {
+
+                System.out.print("*");c
+        }
+            System.out.println();
+        }
+    }
+}
