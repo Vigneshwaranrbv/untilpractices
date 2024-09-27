fb9bf45a16b0117f020d5378f9d13aae03dd9aea
Vigneshwaran918@gmail.com <bvigneshwaran918@gmail.com>
2024-09-27 12:32:48 +0530

    type hi

--- src/main/java/org/example/nestedfor.java
+++ src/main/java/org/example/nestedfor.java
@@ -8,7 +8,7 @@ public class nestedfor {
         for (int i = 1; i <= 3; i++) {
 
                 System.out.print("*");
-            System.out.println("Hello world");
+            System.out.println("Hi Hello");
         }
             System.out.println();
         }
